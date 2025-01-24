1ae5713eecd12383aa686d8b471e1f3c8c1f91a1
German Mikheev <mikheev.german@icloud.com>
2025-01-24 15:03:15 +0300

    v0.2 - something made here

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ import json
 import telebot
 from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
+## main functions
+
 # read from json file
 def read_json(filename:str):
   fp = open(filename + ".json", "r")
@@ -21,45 +23,139 @@ def read_json(filename:str):
   fp.close()
   return tmp
 
+# function for creating buttons from current position in 
+def create_buttons_from(current_position):
+  return [KeyboardButton(theme) for theme in current_position]
+
+# get themes names 
+def themes_names(themes):
+  return list(themes.keys())
+
+# JSON files opening
 config = read_json("config")
 all_themes = read_json("themes")
 scripts = read_json("scripts")
 
+
 bot = telebot.TeleBot(config["token"])
 
-### spec_commas ###
-start_hello_comma = """
-enter hello comma here
-"""
 
-# back button text
+# тут хранится текущий открытый раздел
+global current_pos
+current_pos = all_themes
+
+###################
+start_hello_comma = scripts["start_hello_command"]
 bck = scripts["back"]
 
 want_to_solve_tasks = scripts["solve_tasks"]
 want_to_read_theory = scripts["read_theory"]
 ###################
 
+
+
 # start comma
 @bot.message_handler(commands=["start"])
 def start(message):
-  bot.send_message(message.chat.id, start_hello_comma)
+  bot.send_message(message.chat.id, "Привет, " + message.from_user.first_name + "!\n" + start_hello_comma)
+  main_menu(message)
+
 
 @bot.message_handler(commands=["choose"])
 def main_menu(message):
   keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-  themes = list(all_themes.keys())
-  buttons = [KeyboardButton(theme) for theme in themes]
+  buttons = create_buttons_from(current_pos)
   keyboard.add(*buttons)
   keyboard.add(KeyboardButton(bck))
-
-  # keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-  # # Кнопка для решения задач
-  # keyboard.add(KeyboardButton(want_to_solve_tasks))
-  # # Кнопка для просмотра теории
-  # keyboard.add(KeyboardButton(want_to_read_theory))
   bot.send_message(message.chat.id, "Выбери интересующий тебя раздел:", reply_markup=keyboard)
 
-# @bot.message_handler(func=lambda message: message.text )
+
+# @bot.message_handler(func=lambda message: message.text in list(current_pos.keys()))
+# def find_next_themes(message):
+#   print(message.from_user.username, "pressed", message.text)
+#   global current_pos
+#   current_pos = current_pos[str(message.text)]
+#   bot.send_message(message.chat.id, *current_pos)
+  
+
+###################
+# желательно переписать
+
+# раздел 1
+@bot.message_handler(func=lambda message: message.text == "Механика")
+def mechanics_theme(message):
+  keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+  buttons = create_buttons_from(all_themes[message.text])
+  keyboard.add(*buttons)
+  keyboard.add(KeyboardButton(bck))
+  bot.send_message(message.chat.id, f"Выбери интересующий тебя подраздел в теме {message.text}:", reply_markup=keyboard)
+
+
+# кинематика
+@bot.message_handler(func=lambda message: message.text == "Кинематика")
+def kinematics_mechanics_subtheme(message):
+  keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+  buttons = create_buttons_from(all_themes["Механика"]["Кинематика"])
+  keyboard.add(*buttons)
+  keyboard.add(KeyboardButton(bck))
+  bot.send_message(message.chat.id, f"Что будем делать в разделе {message.text}?", reply_markup=keyboard)
+  
+# динамика
+@bot.message_handler(func=lambda message: message.text == "Динамика")
+def dynamics_mechanics_subtheme(message):
+  keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+  buttons = create_buttons_from(all_themes["Механика"]["Динамика"])
+  keyboard.add(*buttons)
+  keyboard.add(KeyboardButton(bck))
+  bot.send_message(message.chat.id, f"Что будем делать в разделе {message.text}?", reply_markup=keyboard)
+
+# статика
+@bot.message_handler(func=lambda message: message.text == "Статика")
+def statics_mechanics_subtheme(message):
+  keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+  buttons = create_buttons_from(all_themes["Механика"]["Статика"])
+  keyboard.add(*buttons)
+  keyboard.add(KeyboardButton(bck))
+  bot.send_message(message.chat.id, f"Что будем делать в разделе {message.text}?", reply_markup=keyboard)
+
+# закон сохранения в механике
+@bot.message_handler(func=lambda message: message.text == "Закон сохранения в механике")
+def zsi_mechanics_subtheme(message):
+  keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+  buttons = create_buttons_from(all_themes["Механика"]["Закон сохранения в механике"])
+  keyboard.add(*buttons)
+  keyboard.add(KeyboardButton(bck))
+  bot.send_message(message.chat.id, f"Что будем делать в разделе {message.text}?", reply_markup=keyboard)
+
+# механические колебания и волны
+@bot.message_handler(func=lambda message: message.text == "Механические колебания и волны")
+def mechanical_koleb_mechanics_subtheme(message):
+  keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+  buttons = create_buttons_from(all_themes["Механика"]["Механические колебания и волны"])
+  keyboard.add(*buttons)
+  keyboard.add(KeyboardButton(bck))
+  bot.send_message(message.chat.id, f"Что будем делать в разделе {message.text}?", reply_markup=keyboard)
+
+
+
+# раздел 2
+@bot.message_handler(func=lambda message: message.text == "Электродинамика")
+def electrodynamics_theme(message):
+  pass
+
+
+# раздел 3
+@bot.message_handler(func=lambda message: message.text == "Квантовая физика")
+def quantum_physics_theme(message):
+  pass
+
+
+# раздел 4
+@bot.message_handler(func=lambda message: message.text == "Молекулярная физика. Термодинамика")
+def molecular_physics_and_thermodynamics_theme(message):
+  pass
+#
+###################
 
 
 # выбор тем задач
@@ -67,13 +163,17 @@ def main_menu(message):
 def tasks_topics(message):
   bot.send_message(message.chat.id, "TASK_OPEN")
 
+
 # выбор тем теории
 @bot.message_handler(func=lambda message: message.text in want_to_read_theory)
 def themes_topics(message):
   bot.send_message(message.chat.id, "THEORY_OPEN")
 
+
 @bot.message_handler(func=lambda message: message.text == bck)
 def return_back(message):
-  pass
+  main_menu(message)
 
-bot.polling(none_stop=True, interval=0, timeout=20)
+
+# bot.polling(none_stop=True, interval=0, timeout=20)
+bot.polling(none_stop=True)
