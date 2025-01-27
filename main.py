f6d64bfb7da40d0905e93e076827d0e8ad7ad09c
German Mikheev <mikheev.german@icloud.com>
2025-01-27 09:48:37 +0300

    v0.3 - working option without tasks/theory

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ def read_json(filename:str):
   fp.close()
   return tmp
 
+# полностью переписать
 # function for creating buttons from current position in 
 def create_buttons_from(current_position):
   return [KeyboardButton(theme) for theme in current_position]
@@ -31,131 +32,82 @@ def create_buttons_from(current_position):
 def themes_names(themes):
   return list(themes.keys())
 
+def create_keyboard(current_buttons_names):
+  keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+  buttons = create_buttons_from(current_buttons_names)
+  keyboard.add(*buttons)
+  keyboard.add(KeyboardButton(back))
+  return keyboard
+
 # JSON files opening
 config = read_json("config")
 all_themes = read_json("themes")
 scripts = read_json("scripts")
 
-
 bot = telebot.TeleBot(config["token"])
-
-
-# тут хранится текущий открытый раздел
-global current_pos
-current_pos = all_themes
 
 ###################
 start_hello_comma = scripts["start_hello_command"]
-bck = scripts["back"]
+back = scripts["back"]
 
 want_to_solve_tasks = scripts["solve_tasks"]
 want_to_read_theory = scripts["read_theory"]
 ###################
 
+# для обработки разных запросов от разных пользователей
+# user position saving
+requests_by_users = {}
 
+path = []
+
+answers = []
+
+def get_by_path():
+  current = all_themes
+  i = 0
+  while (i < len(path)):
+    current = current[path[i]]
+    i += 1
+  return current
+
+def update(message):
+  keyboard = create_keyboard(get_by_path())
+  if len(answers) == 0:
+    main_menu(message)
+    return
+  answer = answers[-1]
+  print("DEBUG:", keyboard, answer)
+  bot.send_message(message.chat.id, answer, reply_markup=keyboard)
+ 
+  
+# сделать для каждого message.chat.id отдельный path и отдельный answers
+# обработка задач (сделать обработчик), сами задачи через метод get_by_path()
 
 # start comma
 @bot.message_handler(commands=["start"])
 def start(message):
   bot.send_message(message.chat.id, "Привет, " + message.from_user.first_name + "!\n" + start_hello_comma)
   main_menu(message)
+  print(f"DEBUG: {message.chat.id}:{message.from_user.username} sent a request: {message.text}")
 
 
+### переписать через inline keyboard button
 @bot.message_handler(commands=["choose"])
 def main_menu(message):
-  keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-  buttons = create_buttons_from(current_pos)
-  keyboard.add(*buttons)
-  keyboard.add(KeyboardButton(bck))
-  bot.send_message(message.chat.id, "Выбери интересующий тебя раздел:", reply_markup=keyboard)
+  answer = "Выбери интересующий тебя раздел:"
+  answers.append(answer)
+  update(message)
 
 
-# @bot.message_handler(func=lambda message: message.text in list(current_pos.keys()))
-# def find_next_themes(message):
-#   print(message.from_user.username, "pressed", message.text)
-#   global current_pos
-#   current_pos = current_pos[str(message.text)]
-#   bot.send_message(message.chat.id, *current_pos)
-  
-
-###################
-# желательно переписать
-
-# раздел 1
-@bot.message_handler(func=lambda message: message.text == "Механика")
-def mechanics_theme(message):
-  keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-  buttons = create_buttons_from(all_themes[message.text])
-  keyboard.add(*buttons)
-  keyboard.add(KeyboardButton(bck))
-  bot.send_message(message.chat.id, f"Выбери интересующий тебя подраздел в теме {message.text}:", reply_markup=keyboard)
+# Функция для выбора подразделов
+@bot.message_handler(func=lambda message: message.text in get_by_path())
+def theme(message):
+  path.append(message.text)
+  answer = f"Выбери интересующий тебя подраздел в теме '{message.text}':"
+  answers.append(answer)
+  update(message)
 
 
-# кинематика
-@bot.message_handler(func=lambda message: message.text == "Кинематика")
-def kinematics_mechanics_subtheme(message):
-  keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-  buttons = create_buttons_from(all_themes["Механика"]["Кинематика"])
-  keyboard.add(*buttons)
-  keyboard.add(KeyboardButton(bck))
-  bot.send_message(message.chat.id, f"Что будем делать в разделе {message.text}?", reply_markup=keyboard)
-  
-# динамика
-@bot.message_handler(func=lambda message: message.text == "Динамика")
-def dynamics_mechanics_subtheme(message):
-  keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-  buttons = create_buttons_from(all_themes["Механика"]["Динамика"])
-  keyboard.add(*buttons)
-  keyboard.add(KeyboardButton(bck))
-  bot.send_message(message.chat.id, f"Что будем делать в разделе {message.text}?", reply_markup=keyboard)
-
-# статика
-@bot.message_handler(func=lambda message: message.text == "Статика")
-def statics_mechanics_subtheme(message):
-  keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-  buttons = create_buttons_from(all_themes["Механика"]["Статика"])
-  keyboard.add(*buttons)
-  keyboard.add(KeyboardButton(bck))
-  bot.send_message(message.chat.id, f"Что будем делать в разделе {message.text}?", reply_markup=keyboard)
-
-# закон сохранения в механике
-@bot.message_handler(func=lambda message: message.text == "Закон сохранения в механике")
-def zsi_mechanics_subtheme(message):
-  keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-  buttons = create_buttons_from(all_themes["Механика"]["Закон сохранения в механике"])
-  keyboard.add(*buttons)
-  keyboard.add(KeyboardButton(bck))
-  bot.send_message(message.chat.id, f"Что будем делать в разделе {message.text}?", reply_markup=keyboard)
-
-# механические колебания и волны
-@bot.message_handler(func=lambda message: message.text == "Механические колебания и волны")
-def mechanical_koleb_mechanics_subtheme(message):
-  keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-  buttons = create_buttons_from(all_themes["Механика"]["Механические колебания и волны"])
-  keyboard.add(*buttons)
-  keyboard.add(KeyboardButton(bck))
-  bot.send_message(message.chat.id, f"Что будем делать в разделе {message.text}?", reply_markup=keyboard)
-
-
-
-# раздел 2
-@bot.message_handler(func=lambda message: message.text == "Электродинамика")
-def electrodynamics_theme(message):
-  pass
-
-
-# раздел 3
-@bot.message_handler(func=lambda message: message.text == "Квантовая физика")
-def quantum_physics_theme(message):
-  pass
-
-
-# раздел 4
-@bot.message_handler(func=lambda message: message.text == "Молекулярная физика. Термодинамика")
-def molecular_physics_and_thermodynamics_theme(message):
-  pass
-#
-###################
 
 
 # выбор тем задач
@@ -170,9 +122,15 @@ def themes_topics(message):
   bot.send_message(message.chat.id, "THEORY_OPEN")
 
 
-@bot.message_handler(func=lambda message: message.text == bck)
+# Откат на одну позицию назад (кнопка BACK)
+@bot.message_handler(func=lambda message: message.text == back)
 def return_back(message):
-  main_menu(message)
+  if (len(path) > 0):
+    path.pop()
+  if (len(answers) > 1):
+    answers.pop()
+  update(message)
+  
 
 
 # bot.polling(none_stop=True, interval=0, timeout=20)
