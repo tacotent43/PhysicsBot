dd0880e19a5e9e26c2a141199173b49096c746a9
German Mikheev <mikheev.german@icloud.com>
2025-01-28 21:50:04 +0300

    v0.4.1 - tasks buttons made - topics buttons made - multiusers support

--- main.py
+++ main.py
@@ -23,7 +23,6 @@ def read_json(filename:str):
   fp.close()
   return tmp
 
-# полностью переписать
 # function for creating buttons from current position in 
 def create_buttons_from(current_position):
   return [KeyboardButton(theme) for theme in current_position]
@@ -56,13 +55,21 @@ want_to_read_theory = scripts["read_theory"]
 
 # для обработки разных запросов от разных пользователей
 # user position saving
-requests_by_users = {}
+users = {}
 
-path = []
 
-answers = []
+def get_user(id):
+  global users
+  if id not in users:
+    users[id] = [
+      [], # [0] - paths
+      []  # [1] - answers
+    ]
+  return users[id]
 
-def get_by_path():
+
+def get_by_path(message):
+  path = get_user(message.chat.id)[0]
   current = all_themes
   i = 0
   while (i < len(path)):
@@ -70,18 +77,42 @@ def get_by_path():
     i += 1
   return current
 
+
 def update(message):
-  keyboard = create_keyboard(get_by_path())
+  path, answers = get_user(message.chat.id)
+  tmp = get_by_path(message)
+  
+  if (len(path) and path[-1] == "Теория"):
+    keyboard = create_keyboard([])
+    bot.send_message(message.chat.id, tmp, reply_markup=keyboard)
+    print("THEORY_DEBUG:", message.chat.id, keyboard)
+    
+    return 
+  
+  if (len(path) and path[-1] == "Задачи"):
+    keyboard = create_keyboard([])
+    # TODO: исправить отображение имен числительных
+    bot.send_message(message.chat.id, f"В базе {len(tmp)} задач по теме {path[-2]}", reply_markup=keyboard)  
+    print("TASKS_DEBUG:", message.chat.id, keyboard)
+    
+    return
+  
+  # обновление клавиатуры
+  keyboard = create_keyboard(tmp)
+  # было ли главное меню
   if len(answers) == 0:
     main_menu(message)
     return
+  
   answer = answers[-1]
-  print("DEBUG:", keyboard, answer)
+  
+  print("DEBUG:", message.chat.id, keyboard, answer)
+  
   bot.send_message(message.chat.id, answer, reply_markup=keyboard)
 
 
 # сделать для каждого message.chat.id отдельный path и отдельный answers
-# обработка задач (сделать обработчик), сами задачи через метод get_by_path()
+# обработка задач (сделать обработчик), сами задачи через метод get_by_path(message)
 
 # start comma
 @bot.message_handler(commands=["start"])
@@ -91,44 +122,38 @@ def start(message):
   print(f"DEBUG: {message.chat.id}:{message.from_user.username} sent a request: {message.text}")
 
 
-### переписать через inline keyboard button
 @bot.message_handler(commands=["choose"])
 def main_menu(message):
   answer = "Выбери интересующий тебя раздел:"
-  answers.append(answer)
+  get_user(message.chat.id)[1].append(answer)
   update(message)
 
 
 # Функция для выбора подразделов
-@bot.message_handler(func=lambda message: message.text in get_by_path())
+@bot.message_handler(func=lambda message: message.text in get_by_path(message))
 def theme(message):
-  path.append(message.text)
+  get_user(message.chat.id)[0].append(message.text)
   answer = f"Выбери интересующий тебя подраздел в теме '{message.text}':"
-  answers.append(answer)
+  get_user(message.chat.id)[1].append(answer)
   update(message)
 
 
+"""
+TODO: 
+Сделать классификацию по разделам 
+Реализовать систему вывода задач
+Реализовать систему проверки задач
+"""
 
-
-# выбор тем задач
-@bot.message_handler(func=lambda message: message.text in want_to_solve_tasks)
-def tasks_topics(message):
-  bot.send_message(message.chat.id, "TASK_OPEN")
-
-
-# выбор тем теории
-@bot.message_handler(func=lambda message: message.text in want_to_read_theory)
-def themes_topics(message):
-  bot.send_message(message.chat.id, "THEORY_OPEN")
 
 
 # Откат на одну позицию назад (кнопка BACK)
 @bot.message_handler(func=lambda message: message.text == back)
 def return_back(message):
-  if (len(path) > 0):
-    path.pop()
-  if (len(answers) > 1):
-    answers.pop()
+  if (len(get_user(message.chat.id)[0]) > 0):
+    get_user(message.chat.id)[0].pop()
+  if (len(get_user(message.chat.id)[1]) > 1):
+    get_user(message.chat.id)[1].pop()
   update(message)
   
 
